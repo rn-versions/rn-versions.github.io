2b299d1cf1e2b91becbd8da7694f09e5d4221a09
Nick Gerleman <ngerlem@microsoft.com>
2021-12-28 09:21:53 -0800

    Header and mobile polish

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -10,7 +10,6 @@ import {
   Link,
   Pivot,
   PivotItem,
-  Stack,
   Text,
   ThemeProvider,
 } from "@fluentui/react";
@@ -30,65 +29,55 @@ export type NavPivotItem<ItemKey extends string> = {
 };
 
 const NavBar = <ItemKey extends string>(props: NavBarProps<ItemKey>) => (
-  <>
-    <ThemeProvider theme={props.theme ?? darkTheme}>
-      <Stack
-        horizontal
-        className={styles.nav}
-        style={{
-          backgroundColor: (props.theme ?? darkTheme).semanticColors
-            .bodyBackground,
+  <ThemeProvider
+    className={styles.nav}
+    theme={props.theme ?? darkTheme}
+    style={{
+      backgroundColor: (props.theme ?? darkTheme).semanticColors.bodyBackground,
+    }}
+  >
+    <div className={styles.navContent}>
+      <div className={styles.brand}>
+        <ReactLogoIcon className={styles.reactLogo} />
+        <Text variant="large">React Native Versions</Text>
+      </div>
+
+      <Pivot
+        headersOnly
+        className={styles.pivot}
+        onLinkClick={(item) => {
+          window.scrollTo({ left: 0, top: 0, behavior: "smooth" });
+          if (props.onItemSelected) {
+            props.onItemSelected(item!.props.itemKey! as ItemKey);
+          }
         }}
       >
-        <div className={styles.navContent}>
-          <div className={styles.brand}>
-            <ReactLogoIcon className={styles.reactLogo} />
-            <Text variant="large">React Native Versions</Text>
-          </div>
+        {props.items.map((p) => (
+          <PivotItem headerText={p.label} itemKey={p.key} />
+        ))}
+      </Pivot>
 
-          <Pivot
-            headersOnly
-            className={styles.pivot}
-            onLinkClick={(item) => {
-              window.scrollTo({ left: 0, top: 0, behavior: "smooth" });
-              if (props.onItemSelected) {
-                props.onItemSelected(item!.props.itemKey! as ItemKey);
-              }
-            }}
-          >
-            {props.items.map((p) => (
-              <PivotItem headerText={p.label} itemKey={p.key} />
-            ))}
-          </Pivot>
-
-          <Link
-            className={styles.gitHubLink}
-            underline={false}
-            href="https://github.com/rn-versions/rn-versions.github.io"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <ActionButton
-              className={styles.gitHubTextButton}
-              text="Contribute"
-              aria-label="Contribute"
-              onRenderIcon={() => (
-                <GitHubLogoIcon className={styles.gitHubLogo} />
-              )}
-            />
-            <IconButton
-              className={styles.gitHubIconButton}
-              aria-label="Contribute"
-              onRenderIcon={() => (
-                <GitHubLogoIcon className={styles.gitHubLogo} />
-              )}
-            />
-          </Link>
-        </div>
-      </Stack>
-    </ThemeProvider>
-    <div className={styles.navSpacer} />
-  </>
+      <Link
+        className={styles.gitHubLink}
+        underline={false}
+        href="https://github.com/rn-versions/rn-versions.github.io"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <ActionButton
+          className={styles.gitHubTextButton}
+          text="Contribute"
+          aria-label="Contribute"
+          onRenderIcon={() => <GitHubLogoIcon className={styles.gitHubLogo} />}
+        />
+        <IconButton
+          className={styles.gitHubIconButton}
+          aria-label="Contribute"
+          onRenderIcon={() => <GitHubLogoIcon className={styles.gitHubLogo} />}
+        />
+      </Link>
+    </div>
+  </ThemeProvider>
 );
 
 export default NavBar;
