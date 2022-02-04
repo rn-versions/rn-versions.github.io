956ed78c2a81302a7ac4e64da9c213cad3ecef38
Bartosz Kaszubowski <gosimek@gmail.com>
2022-02-04 15:03:48 +0100

    allow user to turn on and off chart series

--- src/components/VersionLegend.tsx
+++ src/components/VersionLegend.tsx
@@ -7,21 +7,28 @@ import { colorForHue } from "../chartColor";
 type VersionLegendProps = {
   payload: Payload[];
   versionHues: Record<string, number>;
+  hiddenSeries: string[];
+  setHiddenSeries: React.Dispatch<React.SetStateAction<string[]>>;
 };
 
 const VersionLegend: React.FC<VersionLegendProps> = ({
   payload,
   versionHues,
+  hiddenSeries,
+  setHiddenSeries,
 }) => {
   return (
     <ThemeContext.Consumer>
       {(theme) => (
         <div className={styles.versionsListContainer}>
           <ul className={styles.versionsList}>
-            {payload.map((entry) => {
-              const colorChipColor = theme
+            {payload.map(({ value, color }) => {
+              const isHidden = hiddenSeries.includes(value);
+              const colorChipColor = isHidden
+                ? theme?.semanticColors.buttonBackgroundDisabled
+                : theme
                 ? colorForHue(
-                    versionHues[entry.value!],
+                    versionHues[value!],
                     theme.isInverted
                       ? { variant: "dark", targetLuminance: "contrasts-dark" }
                       : {
@@ -29,16 +36,34 @@ const VersionLegend: React.FC<VersionLegendProps> = ({
                           targetLuminance: "contrasts-light",
                         }
                   )
-                : entry.color!;
+                : color!;
 
               return (
-                <li key={entry.value} className={styles.versionsListItem}>
+                <li
+                  key={value}
+                  className={styles.versionsListItem}
+                  onClick={() =>
+                    setHiddenSeries((hiddenSeries: string[]) =>
+                      hiddenSeries.includes(value)
+                        ? hiddenSeries.filter((key: string) => key !== value)
+                        : hiddenSeries.concat(value)
+                    )
+                  }
+                >
                   <div
                     className={styles.versionColorIndicator}
                     style={{ backgroundColor: colorChipColor }}
                   />
-                  <Text variant="small" className={styles.versionLabel}>
-                    {entry.value}
+                  <Text
+                    variant="small"
+                    className={styles.versionLabel}
+                    style={
+                      isHidden
+                        ? { color: theme?.semanticColors.buttonTextDisabled }
+                        : {}
+                    }
+                  >
+                    {value}
                   </Text>
                 </li>
               );
