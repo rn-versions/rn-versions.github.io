5295d8c421735392599be4c126f5499811ad48fc
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-07-19 00:29:39 +0200

    Fix react and @types/react version mismatch and "Warning: You are importing createRoot from "react-dom" which is not supported. You should instead import it from "react-dom/client"." (#44)

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import HistoryReader from "./HistoryReader";
 
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./components/App";
 
 import type {} from "react-dom/experimental";
 
 HistoryReader.prefetch();
 
-ReactDOM.createRoot(document.getElementById("app")!).render(
+createRoot(document.getElementById("app")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
