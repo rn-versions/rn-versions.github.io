4541da025b81a250ea1e1d8f8eb11cefcde37069
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-07-19 21:48:10 +0200

    Remove react-dom/experimental imports (#45)

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App";
 
-import type {} from "react-dom/experimental";
-
 HistoryReader.prefetch();
 
 createRoot(document.getElementById("app")!).render(
